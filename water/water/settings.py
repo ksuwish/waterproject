e8801b7383b6e062d9e540283522deaee0cc379c
Adem <ademsevinc82@gmail.com>
2024-07-24 16:25:10 +0300

    media düzenlenmesi

--- water/water/settings.py
+++ water/water/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "userpage"
 LOGOUT_REDIRECT_URL ="login"
+
+MEDIA_URL = '/media/products/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '')
--- water/waterproject/urls.py
+++ water/waterproject/urls.py
@@ -2,12 +2,14 @@ from django.urls import path, include
 from . import views
 from .views import authView, CustomLoginView
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path("",views.index),
-    path("index",views.index),
+    path("", views.index),
+    path("index", views.index),
     path('blogs/', views.blogs, name='blogs'),
-    path("accounts/signup/", authView, name= "authView"),
+    path("accounts/signup/", authView, name="authView"),
     path("accounts/", include("django.contrib.auth.urls")),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('superuser_dashboard/', views.superuser_dashboard, name='superuser_dashboard'),
@@ -15,3 +17,6 @@ urlpatterns = [
     path('user_dashboard/', views.user_dashboard, name='user_dashboard'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
